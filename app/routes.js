3fddf810ccf691c9a9c9240707f783a2a5ef0dcb
javaknight1 <robertaveryiv@gmail.com>
2015-03-04 12:42:51 -0500

    Fully capable of uploading files and stored them to database

--- app/routes.js
+++ app/routes.js
@@ -4,7 +4,8 @@ var fs          = require("fs-extra");
 var mysql       = require('mysql');
 var dbconfig    = require('../config/database');
 var connection  = mysql.createConnection(dbconfig.connection);
-var ffmpeg      = require('fluent-ffmpeg');
+//var ffmpeg      = require('fluent-ffmpeg');
+var ffmpeg      = require('liquid-ffmpeg');
 
 connection.query('USE ' + dbconfig.database);
 
@@ -77,11 +78,6 @@ module.exports = function(app, passport) {
     // Process to upload film file
     app.post('/upload/film', function(req, res){
         fs.exists(req.files.film_file.path, function(exists) {
-
-            //convert to all three formats (ogv, mp4, and webm)
-            //move new files into uploads/film folder
-            //add info to film_file
-
             req.files.film_file.kind = 0;
             updateDraft(req.user.id, req.files.film_file, null);
         });
@@ -199,7 +195,7 @@ function createRandomName(){
     var text = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    for( var i=0; i < 5; i++ )
+    for( var i=0; i < 20; i++ )
         text += possible.charAt(Math.floor(Math.random() * possible.length));
 
     return text;
@@ -207,23 +203,39 @@ function createRandomName(){
 
 function updateDraft(filmmakerId, upload, info){
 
+    console.log("Searching database...");
     connection.query("SELECT * FROM films WHERE filmmakerId = ? AND draft = 1", [filmmakerId], function(err, rows){
+
+        if(err)
+            throw "Error finding draft.";
+
        //draft does not exist
        if(rows.length == 0){
+           console.log("Searched database with no record");
            //insert draft into table
             connection.query("INSERT INTO films(filmmakerId) VALUE(?)", [filmmakerId], function(err, rows){
+
+                if(err)
+                    throw "Error adding draft.";
+
+                console.log("Added new draft");
                 if(upload != null){
-
-
-
+                    updateUploadInfo(rows.insertId, upload);
                 }
 
                 if(info != null){
-
+                    updateFilmInfo(rows.insertId, info);
                 }
             });
        }else{
+           console.log("Searched database with record");
+           if(upload != null){
+               updateUploadInfo(rows[0].filmId, upload);
+           }
 
+           if(info != null){
+               updateFilmInfo(rows[0].filmId, info);
+           }
        }
     });
 }
@@ -233,24 +245,80 @@ function updateFilmInfo(id, info){
 
 }
 
-function updateUploadInfo(filmId, upload){
+function updateUploadInfo(id, upload){
 
     //create three random names
-    var ogv_name = createRandomName();
     var mp4_name = createRandomName();
+    var ogv_name = createRandomName();
     var webm_name = createRandomName();
 
-    //Craete mp4 file
-    ffmpeg(req.files.film_file.path).output('uploads/tmp/file_convert.mp4').audioCodec('libfaac').audioBitrate('96k').videoCodec('libx264').toFormat('mp4').size('640x360');
-    //convert to ogv file
-    ffmpeg(req.files.film_file.path).output('uploads/tmp/file_convert.ogv').audioCodec('libvorbis').audioBitrate('96k').videoCodec('libtheora').toFormat('ogg').size('640x360');
-    //convert to webm file
-    ffmpeg(req.files.film_file.path).output('uploads/tmp/file_convert.webm').audioCodec('libvorbis').audioBitrate('96k').videoCodec('libvpx').toFormat('webm').size('640x360');
+    //convert to mp4 file
+    console.log("Creating mp4 file");
+    var proc_mp4 = new ffmpeg({ source: upload.path, timeout: 10000})
+        .withVideoCodec('libx264')
+        .withVideoBitrate('1500k')
+        .withAudioCodec('libfaac')
+        .withAudioBitrate('96k')
+        .withSize("640x360")
+        .toFormat('mp4')
+        .onProgress(function(progress) {
+            //console.log("MP4 format progress ..." + util.inspect(progress));
+        })
+        .saveToFile("uploads/"+mp4_name+".mp4", function(stdout, stderr) {
+            console.log('File [' + mp4_name + ']converted to mp4 format!!');
+            //insert into uploads table once converted
+            connection.query("INSERT INTO uploads(filmId, name, type, mime) VALUES(?, ?, ?, 'mp4')", [id, mp4_name, upload.kind], function(err, rows){
+                if(err) {
+                    throw "Err: " + err + ": Could not record mp4 file[" + mp4_name + "]";
+                }
 
-    //insert mp4 file info into upload
-    connection.query("INSERT INTO uploads(filmId, name, type, mime) VALUES(?, ?, ?, mp4)", [filmId, , ], function(err, rows){});
-    //insert ogv file info into upload
-    connection.query("INSERT INTO uploads(filmId, name, type, mime) VALUES(?, ?, ?, ogv)", [filmId, , ], function(err, rows){});
-    //insert webm file info into upload
-    connection.query("INSERT INTO uploads(filmId, name, type, mime) VALUES(?, ?, ?, webm)", [filmId, , ], function(err, rows){});
+                console.log("Recorded mp4 file [" + mp4_name + "]");
+                //convert to ogv file
+                console.log("Creating ogg file");
+                var proc_ogg = new ffmpeg({ source: upload.path, timeout: 10000 })
+                    .withVideoCodec('libtheora')
+                    .withVideoBitrate('1500k')
+                    .withAudioCodec('libvorbis')
+                    .withAudioBitrate('96k')
+                    .withSize("640x360")
+                    .toFormat('ogg')
+                    .onProgress(function(progress) {
+                        //console.log("OGG format progress ..." + progress);
+                    })
+                    .saveToFile("uploads/"+ogv_name+".ogv", function(stdout, stderr) {
+                        console.log('File converted to ogg format!!');
+                        //insert into uploads table once converted
+                        connection.query("INSERT INTO uploads(filmId, name, type, mime) VALUES(?, ?, ?, 'ogv')", [id, ogv_name, upload.kind], function(err, rows){
+                            if(err) {
+                                throw "Err: " + err + ": Could not record ogv file[" + ogv_name + "]";
+                            }
+
+                            console.log("Recorded ogv file [" + ogv_name + "]");
+                            //convert to webm format
+                            console.log("Creating webm file");
+                            var proc_webm = new ffmpeg({ source: upload.path, timeout: 10000 })
+                                .withVideoCodec('libvpx')
+                                .withVideoBitrate('1500k')
+                                .withAudioCodec('libvorbis')
+                                .withAudioBitrate('96k')
+                                .withSize("640x360")
+                                .toFormat('webm')
+                                .onProgress(function(progress) {
+                                    //console.log("WEBM format progress ..." + progress);
+                                })
+                                .saveToFile("uploads/"+webm_name+".webm", function(stdout, stderr) {
+                                    console.log('File converted to webm format!!');
+                                    //insert into uploads table once converted
+                                    connection.query("INSERT INTO uploads(filmId, name, type, mime) VALUES(?, ?, ?, 'webm')", [id, webm_name, upload.kind], function(err, rows){
+                                        if(err) {
+                                            throw "Err: " + err + ": Could not record webm file[" + webm_name + "]";
+                                        }
+
+                                        console.log("Recorded webm file [" + webm_name + "]");
+                                    });
+                                });
+                        });
+                    });
+            });
+        });
 }
